966ef1767232bafd623f9491789070f0441fe734
gilsonvarghese27 <gilson.v@rently.com>
2019-04-30 17:21:50 +0530

    Added chai.js

--- testing_frameworks/mocha/calc/test.js
+++ testing_frameworks/mocha/calc/test.js
@@ -1,6 +1,11 @@
 const operations = require('./operations');
 const assert = require('assert');
+const expect = require('chai').expect;
 
 it('should return true', () => {
-    assert.equal(operations.add(3, 1), 2);
+    assert.equal(operations.add(3, 1), 4);
+});
+
+it('should add negative number', () => {
+    expect(operations.add(-2, -3)).to.be.equal(-5);
 });
