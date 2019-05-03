8202521a8fafd0ca9a1866d5c7490d3ea1a43f1a
gilsonvarghese27 <gilson.v@rently.com>
2019-05-03 15:10:59 +0530

    Updated reject

--- promises/index.js
+++ promises/index.js
@@ -1,7 +1,13 @@
+const data = null;
 let promise = new Promise((resolve, reject) => {
+    if(data === null) {
+        reject("Failed");
+    }
     resolve("Successfull");
 });
 
 promise.then((value) => {
     console.log("value = " + value);
+}, (err) => {
+    console.error("Error: " + err);
 });
