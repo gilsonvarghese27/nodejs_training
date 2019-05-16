a2020b1ffce3a9111e293e2088a90fbd7f2685bd
gilsonvarghese27 <gilson.v@rently.com>
2019-05-16 13:33:55 +0530

    Added test odule for publishing

---
+++ module_test/index.js
@@ -0,0 +1,8 @@
+/**
+ * @author gilson004
+ * @datetime 15 May 2019
+ */
+
+exports.sum = function sum(a, b) {
+    return a+b;
+}
---
+++ module_test/test.js
@@ -0,0 +1,3 @@
+const sum = require('./index.js');
+
+console.log(sum.sum(4,3));
